547ec3f1fd5bc6fb0625b50ee463b06702e556d4
loseroa <minhchaumvp@gmail.com>
2021-01-03 14:59:28 +0700

    fetch list campaign from server

--- src/app/list-campaign/list-campaign.component.ts
+++ src/app/list-campaign/list-campaign.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import axios from 'axios';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-campaign',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-campaign.component.css']
 })
 export class ListCampaignComponent implements OnInit {
-
+  listCampaign = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.fetchCampaigns()
+  }
+
+  fetchCampaigns(): void {
+    axios.get('http://localhost:3000/api/campaigns/index')
+      .then(resp => {
+          this.listCampaign = resp.data
+      })
+      .catch(err => {
+          // Handle Error Here
+          console.error(err);
+      });
+  }
+
+  getRemainDays(expiredDate: Date): Number {
+    return 0;
   }
 
 }
