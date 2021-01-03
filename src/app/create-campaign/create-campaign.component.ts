83a86f291d3a2978cb6e150931fd79f919298d6e
VietHoang <dvhoang2106@gmail.com>
2021-01-03 13:51:48 +0700

    complete create campaign

--- src/app/create-campaign/create-campaign.component.ts
+++ src/app/create-campaign/create-campaign.component.ts
@@ -6,52 +6,58 @@ const API_URL_CREATE_CAMPAIGN = 'http://localhost:3000/api/campaigns/create';
 @Component({
   selector: 'app-create-campaign',
   templateUrl: './create-campaign.component.html',
-  styleUrls: ['./create-campaign.component.css']
+  styleUrls: ['./create-campaign.component.css'],
 })
-
 export class CreateCampaignComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   campaign = new FormGroup({
     campaignContent: new FormControl(''),
     campaignName: new FormControl(''),
     shortDescription: new FormControl(''),
     goal: new FormControl(''),
     expiredDate: new FormControl(''),
-    groupName: new FormControl('')
+    groupName: new FormControl(''),
   });
   onSubmit() {
-  console.log(this.campaign.value.campaignContent);
-  console.log(this.campaign.value.campaignName);
-  console.log(this.campaign.value.shortDescription);
-  console.log(this.campaign.value.goal);
-  console.log(this.campaign.value.expiredDate);
-  console.log(this.campaign.value.groupName);
-  this.createCampaign(
-    this.campaign.value.campaignName,
-    this.campaign.value.shortDescription,
-    this.campaign.value.expiredDate,
-    this.campaign.value.campaignContent,
-    this.campaign.value.goal
-    )
-  
+    console.log(this.campaign.value.campaignContent);
+    console.log(this.campaign.value.campaignName);
+    console.log(this.campaign.value.shortDescription);
+    console.log(this.campaign.value.goal);
+    console.log(this.campaign.value.expiredDate);
+    console.log(this.campaign.value.groupName);
+    this.createCampaign(
+      this.campaign.value.campaignName,
+      this.campaign.value.shortDescription,
+      this.campaign.value.expiredDate,
+      this.campaign.value.campaignContent,
+      this.campaign.value.goal
+    );
   }
-  createCampaign = (name: any, shortDescription: any ,
-                    expiredDate: any , fullDescription: any, 
-                    goal: any) => {
-    return axios.post(API_URL_CREATE_CAMPAIGN,{
-      name,
-      shortDescription,
-      expiredDate,
-      fullDescription,
-      goal
-
-    } ).then ((response) =>{
-      console.log(response);
-      alert(response.data.message)
-    })
-  }
+  createCampaign = (
+    name: any,
+    shortDescription: any,
+    expiredDate: any,
+    fullDescription: any,
+    goal: any
+  ) => {
+    return axios
+      .post(API_URL_CREATE_CAMPAIGN, {
+        name,
+        shortDescription,
+        expiredDate,
+        fullDescription,
+        goal,
+        statusId: '',
+        categoryId: '',
+        coverImageUrl: '',
+      },
+      { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } } )
+      .then((response) => {
+        console.log(response);
+        alert(response.data.message);
+        window.location.replace('/home')
+      });
+  };
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import  {apiService} from '../sercive/api.service';
+import { apiService } from '../sercive/api.service';
 import { FormGroup, FormControl } from '@angular/forms';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   user = new FormGroup({
     username: new FormControl(''),
     password: new FormControl(''),
@@ -22,17 +20,22 @@ export class SignupComponent implements OnInit {
     console.warn(this.user.value.username);
     console.warn(this.user.value.password);
     console.warn(this.user.value.email);
-    let service = new apiService()
-    service.signup(this.user.value.username, this.user.value.password,this.user.value.email)
-    .then((user)=>{
-      alert(user.message)
-      this.router.navigate(['/login'])
-    },
-    
-     (error) => {
-      alert('SIGNUP_ERROR');
-    }
-    )
-    
+    let service = new apiService();
+    service
+      .signup(
+        this.user.value.username,
+        this.user.value.password,
+        this.user.value.email
+      )
+      .then(
+        (user) => {
+          alert(user.message);
+          this.router.navigate(['/login']);
+        },
+
+        (error) => {
+          alert('SIGNUP_ERROR');
+        }
+      );
   }
 }
